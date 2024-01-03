d3a6665b36fb8d97cdac05a84323827fb801cbd0
tarush-r <tarush.r@somaiya.edu>
2024-01-03 14:15:52 -0800

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Tarush Tejas Rajput on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
